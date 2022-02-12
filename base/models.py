14e2d269201370a092551b188b065bf487d1090f
Samyak-jain09 <samyakjain0902@gmail.com>
2022-02-13 02:37:20 +0530

    Added CRUD functionalities to room

--- base/models.py
+++ base/models.py
@@ -12,13 +12,15 @@ class Topic(models.Model):
         return self.name
 
 class Room(models.Model):
-    # host=
+    host=models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
     topic=models.ForeignKey(Topic, on_delete=models.SET_NULL,null=True)
     name= models.CharField(max_length=200)
     description= models.TextField(null=True, blank= True)
     # participants=
     updated = models.DateTimeField(auto_now=True)
     created= models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering=['-updated', '-created']
 
     def __str__(self):
         return self.name
