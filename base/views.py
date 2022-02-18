94a36d44fac9cffb266c1928835b5de1e4757be1
Samyak-jain09 <samyakjain0902@gmail.com>
2022-02-19 02:26:43 +0530

    Done with login/logout theme

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Room,Topic, Message
 from django.contrib.auth.decorators import login_required
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -153,13 +153,24 @@ def deleteRoom(request,pk):
 
 
 @login_required(login_url='login')
-def deleteMessage(request,pk):
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
 
-    message=Message.objects.get(id=pk)
-    if request.user != message.user :
-        return HttpResponse('You are not allowed here! ')
+    if request.user != message.user:
+        return HttpResponse('Your are not allowed here!!')
 
     if request.method == 'POST':
         message.delete()
-        return redirect('home')
-    return render(request, 'base/delete.html', {'obj':message})
+        return redirect('room', pk= message.room.id)
+    return render(request, 'base/delete.html', {'obj': message})
+
+@login_required(login_url='login')
+def updateUser(request):
+    user=request.user
+    form = UserForm(instance= user)
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance= user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+    return render(request, 'base/update-user.html',{'form':form,})
